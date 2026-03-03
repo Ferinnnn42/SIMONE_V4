650f76223f0fdc096baca3bfa3e82a8f0e461a00
josueportiz <josueportiz@gmail.com>
2026-03-03 10:58:53 +0100

    Fix funtion naming inconsistency

--- example_v3.c
+++ example_v3.c
@@ -31,19 +31,18 @@ int main(void)
         uint8_t b = (uint8_t)(rand() % COLOR_RGB_MAX_VALUE);
         rgb_color_t color_test = {.r = r, .g = g, .b = b};
 
-        // Set a new color
+        // Set a new color and intensity
         printf("Testing color [R, G, B] = [%d, %d, %d]\n", r, g, b);
-        fsm_rgb_light_set_color(p_fsm_rgb_light, color_test);
-        fsm_rgb_light_fire(p_fsm_rgb_light);
-
-        // Set a new intensity
         uint8_t intensity_perc = MAX_LEVEL_INTENSITY;
+        
+        fsm_rgb_light_set_color_intensity(p_fsm_rgb_light, color_test, intensity_perc);
+        fsm_rgb_light_fire(p_fsm_rgb_light);
 
         // While there is something visible
         while((p_fsm_rgb_light->color.r > 1) && (p_fsm_rgb_light->color.g > 1) && (p_fsm_rgb_light->color.b > 1))
         {
             printf("[%ld] Display color at intensity of %d percentage: [R, G, B] = [%d, %d, %d]\n", port_system_get_millis(), intensity_perc, p_fsm_rgb_light->color.r, p_fsm_rgb_light->color.g, p_fsm_rgb_light->color.b);
-            fsm_rgb_light_set_intensity(p_fsm_rgb_light, intensity_perc);
+            fsm_rgb_light_set_color_intensity(p_fsm_rgb_light, p_fsm_rgb_light->color, intensity_perc);
             fsm_rgb_light_fire(p_fsm_rgb_light);
             port_system_delay_ms(10);
             intensity_perc -= 1;
